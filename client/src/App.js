91a5e39e0daee5aaa7c03da98b8d8160cd4548e6
Tyler <nuvallo.inc@gmail.com>
2020-01-24 21:17:29 -0500

    Navigation bar created

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import NavBar from "./components/Navigation";
 import "./App.css";
 
-function App() {
-  return null;
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+      </div>
+    );
+  }
 }
 
 export default App;
