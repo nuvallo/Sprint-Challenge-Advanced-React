5bc457d221a4677a42e024273f9d7caca7138674
Tyler <nuvallo.inc@gmail.com>
2020-01-24 22:25:27 -0500

    Hooks finished and darkmode styled

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,21 +1,31 @@
 import React from "react";
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+import { DarkMode } from "../Hooks/DarkMode";
 
-class NavBar extends React.Component {
-  render() {
-    return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">Player App</NavbarBrand>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Home</NavLink>
-            </NavItem>
-          </Nav>
-        </Navbar>
-      </div>
-    );
-  }
+function NavBar() {
+  const [darkMode, setDarkMode] = DarkMode("darkvalue", false);
+  const toggleSwitch = event => {
+    event.preventDefault();
+    setDarkMode(!darkMode);
+  };
+  return (
+    <div>
+      <Navbar
+        onClick={toggleSwitch}
+        className={darkMode ? "toggle toggled" : "toggle"}
+        color="light"
+        light
+        expand="md"
+      >
+        <NavbarBrand href="/">Player App</NavbarBrand>
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+            <NavLink href="/components/">Home</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </div>
+  );
 }
 
 export default NavBar;
